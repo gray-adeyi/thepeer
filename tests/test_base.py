11217750c287f96c82cbc9a8cb8a257e6bb3d015
Gbenga Adeyi <adeyigbenga005@gmail.com>
2023-09-08 22:45:46 +0100

    Test BaseClient and BaseAsyncClient

---
+++ tests/test_base.py
@@ -0,0 +1,45 @@
+from unittest import TestCase, IsolatedAsyncioTestCase
+
+from thepeer.base import BaseClient
+
+
+class BaseClientTestCase(TestCase):
+    @classmethod
+    def setUpClass(cls) -> None:
+        cls.secret_key = "qwerty"
+        cls.client = BaseClient(cls.secret_key)
+
+    def test_base_url(self):
+        self.assertEqual(
+            self.client.BASE_URL, "https://api.thepeer.co", "BASE_URL has changed"
+        )
+
+    def test_headers(self):
+        expected_headers = {"x-api-key": self.secret_key, "Accept": "Application/json"}
+        self.assertDictEqual(
+            self.client.headers, expected_headers, "expected headers not provided"
+        )
+
+    def test_api_call(self):
+        ...
+
+
+class BaseAsyncClient(IsolatedAsyncioTestCase):
+    @classmethod
+    def setUpClass(cls) -> None:
+        cls.secret_key = "qwerty"
+        cls.client = BaseClient(cls.secret_key)
+
+    def test_base_url(self):
+        self.assertEqual(
+            self.client.BASE_URL, "https://api.thepeer.co", "BASE_URL has changed"
+        )
+
+    def test_headers(self):
+        expected_headers = {"x-api-key": self.secret_key, "Accept": "Application/json"}
+        self.assertDictEqual(
+            self.client.headers, expected_headers, "expected headers not provided"
+        )
+
+    async def test_api_call(self):
+        ...
