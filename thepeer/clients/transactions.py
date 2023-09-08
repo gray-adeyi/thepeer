f12eb0eba528cff897210b0ad5b9df8aa2efeaa3
Gbenga Adeyi <adeyigbenga005@gmail.com>
2023-09-08 23:45:50 +0100

    Add docstrings to clients

--- thepeer/clients/transactions.py
+++ thepeer/clients/transactions.py
@@ -1,19 +1,47 @@
 from typing import Optional
 
 from thepeer.base import BaseClient, BaseAsyncClient
-from thepeer.utils import HTTPMethod
+from thepeer.utils import HTTPMethod, Response
 
 
 class TransactionClient(BaseClient):
     def __init__(self, secret_key: Optional[str] = None):
+        """
+        Args:
+             secret_key: your thepeer secret_key. if not provide.
+                the client tries to access your thepeer secret key
+                from your environmental variables via.
+                THEPEER_SECRET_KEY=<secret_key>. if the secret
+                key is provided on instantiation, it overrides
+                the secret key in the environmental variables.
+        """
         super().__init__(secret_key)
 
-    def get(self, transaction_id: str):
+    def get(self, transaction_id: str) -> Response:
+        """Fetch a transaction.
+
+        Args:
+            transaction_id: The transaction’s identifier
+
+        Returns:
+            Response: A dataclass containing the HTTP status code and
+                data returned by thepeer servers
+        """
         return self.api_call(
             endpoint_path=f"/transactions/{transaction_id}", method=HTTPMethod.GET
         )
 
-    def refund(self, transaction_id: str, reason: str):
+    def refund(self, transaction_id: str, reason: str) -> Response:
+        """Fetch a transaction.
+
+        Args:
+            transaction_id: The transaction’s identifier
+            reason: why the refund is made.
+
+        Returns:
+            Response: A dataclass containing the HTTP status code and
+                data returned by thepeer servers
+        """
         return self.api_call(
             endpoint_path=f"/transactions/{transaction_id}/refund",
             method=HTTPMethod.POST,
@@ -23,14 +51,42 @@ class TransactionClient(BaseClient):
 
 class AsyncTransactionClient(BaseAsyncClient):
     def __init__(self, secret_key: Optional[str] = None):
+        """
+        Args:
+             secret_key: your thepeer secret_key. if not provide.
+                the client tries to access your thepeer secret key
+                from your environmental variables via.
+                THEPEER_SECRET_KEY=<secret_key>. if the secret
+                key is provided on instantiation, it overrides
+                the secret key in the environmental variables.
+        """
         super().__init__(secret_key)
 
-    async def get(self, transaction_id: str):
+    async def get(self, transaction_id: str) -> Response:
+        """Fetch a transaction.
+
+        Args:
+            transaction_id: The transaction’s identifier
+
+        Returns:
+            Response: A dataclass containing the HTTP status code and
+                data returned by thepeer servers
+        """
         return await self.api_call(
             endpoint_path=f"/transactions/{transaction_id}", method=HTTPMethod.GET
         )
 
-    async def refund(self, transaction_id: str, reason: str):
+    async def refund(self, transaction_id: str, reason: str) -> Response:
+        """Fetch a transaction.
+
+        Args:
+            transaction_id: The transaction’s identifier
+            reason: why the refund is made.
+
+        Returns:
+            Response: A dataclass containing the HTTP status code and
+                data returned by thepeer servers
+        """
         return await self.api_call(
             endpoint_path=f"/transactions/{transaction_id}/refund",
             method=HTTPMethod.POST,
--- thepeer/thepeer.py
+++ thepeer/thepeer.py
@@ -8,15 +8,38 @@ from thepeer.utils import ChargeEvent, HTTPMethod, Response, Currency
 
 
 class ThePeerClient(BaseClient):
+    """ThePeerClient provides methods and attributes that serves as a
+    convenience for interacting with thepeer"""
+
     def __init__(self, secret_key: str):
+        """
+        Args:
+             secret_key: your thepeer secret_key. if not provide.
+                the client tries to access your thepeer secret key
+                from your environmental variables via.
+                THEPEER_SECRET_KEY=<secret_key>. if the secret
+                key is provided on instantiation, it overrides
+                the secret key in the environmental variables.
+        """
         super().__init__(secret_key)
         self.users = UserClient(self.secret_key)
         self.transactions = TransactionClient(self.secret_key)
         self.links = LinkClient(self.secret_key)
 
-    def charge(self, reference: str, event: ChargeEvent) -> Response:
+    def charge(self, charge_reference: str, event: ChargeEvent) -> Response:
+        """Process a charge authorization request.
+
+        Arg:
+            charge_reference: The reference of the charge in the authorization payload
+            event: The event is a required string that could be one of four supported
+                values that describes the status of the charge
+
+        Returns:
+            Response: A dataclass containing the HTTP status code and
+                data returned by thepeer servers
+        """
         return self.api_call(
-            endpoint_path=f"/authorization/{reference}",
+            endpoint_path=f"/authorization/{charge_reference}",
             method=HTTPMethod.POST,
             data={"event": event},
         )
@@ -28,7 +51,24 @@ class ThePeerClient(BaseClient):
         redirect_url: Optional[str] = None,
         meta: Optional[dict] = None,
         currency: Currency = Currency.NGN,
-    ):
+    ) -> Response:
+        """Generate a checkout.
+
+        Args:
+            amount: The amount you are debiting the customer.
+                This should be in kobo. The minimum value is 10000
+            email: The customer’s email address
+            redirect_url: The url we should redirect to after the
+                customer completes payment.
+            meta: An dict containing additional attributes you
+                would like to have in your transaction response.
+            currency: The currency the transaction should be
+                carried out in. The supported value is `Currency.NGN`
+
+        Returns:
+            Response: A dataclass containing the HTTP status code and
+                data returned by thepeer servers
+        """
         data = {
             "currency": currency,
             "amount": amount,
@@ -42,13 +82,36 @@ class ThePeerClient(BaseClient):
 
 
 class AsyncThePeerClient(BaseAsyncClient):
+    """AsyncThePeerClient is an async equivalent of ThePeerClient that provides
+    methods and attributes that serves as a convenience for interacting with thepeer."""
+
     def __init__(self, secret_key: str):
+        """
+        Args:
+             secret_key: your thepeer secret_key. if not provide.
+                the client tries to access your thepeer secret key
+                from your environmental variables via.
+                THEPEER_SECRET_KEY=<secret_key>. if the secret
+                key is provided on instantiation, it overrides
+                the secret key in the environmental variables.
+        """
         super().__init__(secret_key)
         self.users = AsyncUserClient(self.secret_key)
         self.transactions = AsyncTransactionClient(self.secret_key)
         self.links = AsyncLinkClient(self.secret_key)
 
     async def charge(self, reference: str, event: ChargeEvent) -> Response:
+        """Process a charge authorization request.
+
+        Arg:
+            charge_reference: The reference of the charge in the authorization payload
+            event: The event is a required string that could be one of four supported
+                values that describes the status of the charge
+
+        Returns:
+            Response: A dataclass containing the HTTP status code and
+                data returned by thepeer servers
+        """
         return await self.api_call(
             endpoint_path=f"/authorization/{reference}",
             method=HTTPMethod.POST,
@@ -62,7 +125,24 @@ class AsyncThePeerClient(BaseAsyncClient):
         redirect_url: Optional[str] = None,
         meta: Optional[dict] = None,
         currency: Currency = Currency.NGN,
-    ):
+    ) -> Response:
+        """Generate a checkout.
+
+        Args:
+            amount: The amount you are debiting the customer.
+                This should be in kobo. The minimum value is 10000
+            email: The customer’s email address
+            redirect_url: The url we should redirect to after the
+                customer completes payment.
+            meta: An dict containing additional attributes you
+                would like to have in your transaction response.
+            currency: The currency the transaction should be
+                carried out in. The supported value is `Currency.NGN`
+
+        Returns:
+            Response: A dataclass containing the HTTP status code and
+                data returned by thepeer servers
+        """
         data = {
             "currency": currency,
             "amount": amount,
