6b22cc7fe8fe7ca6a185e740622683669c60c391
Gbenga Adeyi <adeyigbenga005@gmail.com>
2023-09-14 22:26:22 +0100

    Add test setup

--- tests/test_clients/test_links.py
+++ tests/test_clients/test_links.py
@@ -1,9 +1,19 @@
 from unittest import TestCase, IsolatedAsyncioTestCase
 
+from dotenv import load_dotenv
+
+from thepeer.clients import LinkClient
+
 
 class LinkClientTestCase(TestCase):
+    @classmethod
+    def setUpClass(cls) -> None:
+        load_dotenv()
+        cls.client = LinkClient()
+
     def test_can_get_user_links(self):
-        ...
+        response = self.client.get_user_links("59e86b01-1a0d-4bd3-955c-746686792e04")
+        print(response)
 
     def test_can_get_linked_account(self):
         ...
--- tests/test_clients/test_transactions.py
+++ tests/test_clients/test_transactions.py
@@ -1,7 +1,16 @@
 from unittest import TestCase, IsolatedAsyncioTestCase
 
+from dotenv import load_dotenv
+
+from thepeer.clients import TransactionClient
+
 
 class TransactionClientTestCase(TestCase):
+    @classmethod
+    def setUpClass(cls) -> None:
+        load_dotenv()
+        cls.client = TransactionClient()
+
     def test_can_get(self):
         ...
 
